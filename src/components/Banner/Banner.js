5f7fde3f2ab7a30eaf6049bb57d630c539477821
igormendes <a.igor.mendes@gmail.com>
2023-08-03 19:33:13 -0300

    Remove unecessary files and add header

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import "./Banner.css";
+
+export const Banner = () => {
+    return (
+        <header className="banner">
+            <div id="social-header"></div>
+            <div id="main-header">
+                <img className="logo" src="/images/bebetobetlogo.jpg" alt="Logo BebetoBet" />
+            </div>
+        </header>
+    );
+}
