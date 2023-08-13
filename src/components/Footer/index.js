cfe4f562ce03354467a3c880ccfcd6a7bd6752c6
igormendes <a.igor.mendes@gmail.com>
2023-08-13 15:52:39 -0300

    Add footer on homepage

---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import "./Footer.css";
+
+export const Footer = () => {
+    return (
+        <footer>
+            <img className="logo" src="/images/bebetobetlogo.jpg" alt="Logo BebetoBet" />
+        </footer>
+    );
+}
