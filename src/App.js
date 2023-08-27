6cc3a402b27692c5b9fc14f9b6df5cb5859080ac
igormendes <a.igor.mendes@gmail.com>
2023-08-27 12:51:41 -0300

    Get exemple matchs from api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import React, { useEffect, useState } from "react"
 import Banner from './components/Banner';
 import { Footer } from './components/Footer';
 import List from './components/List';
 
 function App() {
+    const [odds, setOdds] = useState({});
+
+    const fetchOdds = () => {
+        fetch('http://localhost:8080/odds')
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                setOdds(data);
+            })
+        ;
+    }
+
+    useEffect(() => {
+        fetchOdds();
+    }, []);
+
     return (
         <div className="App">
             <Banner />
             <div className='content list-container'>
-                <List />
+                <List odds={odds} />
             </div>
             <Footer />
         </div>
@@ -15,3 +33,4 @@ function App() {
 }
 
 export default App;
+l
